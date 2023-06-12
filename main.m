dcd265b0d648b9a7d5c928fd0b50eb82c576c534
Tony Ruther <tony.ruther@googlemail.com>
2023-06-12 23:02:18 +0100

    gamma, CP  and H  calculated from polynomials from FLUID_PROPS

--- main.m
+++ main.m
@@ -5,18 +5,17 @@ alt=str2double(arg_list{1});
 xm=str2double(arg_list{2});
 dtamb=str2double(arg_list{3});
 
-gamma=1.4;
 R=8.314;
 M=0.0289644;
 
 TAMB=TISA_FROM_ALT(alt) + dtamb;
 PAMB=PAMB_FROM_ALT(alt);
-PTOTAL=PTOTAL_FROM_MACH(xm, PAMB, gamma);
-TTOTAL=TTOTAL_FROM_MACH(xm, TAMB, gamma);
-VSOUND=VSOUND_FROM_TAMB(TAMB, gamma, R, M);
-RHO=RHO_FROM_PAMB_TAMB(PAMB, TAMB, R, M);
+[GAMMA, CP, H, VSOUND, RHO]=FLUID_PROP('dry air', PAMB, TAMB, R, M);
 
-out=['TAMB = ' num2str(TAMB) '°C, PAMB = ' num2str(PAMB) 'Pa, PTOTAL = ' num2str(PTOTAL) 'Pa, TTOTAL = ' num2str(TTOTAL) '°C, RHO = ' num2str(RHO) 'kg/m³, VSOUND = ' num2str(VSOUND) 'm/s'];
+[PTOTAL, TTOTAL]=PTTOTAL_FROM_MACH(xm, PAMB, TAMB, GAMMA);
+%TTOTAL=TTOTAL_FROM_MACH(xm, TAMB, GAMMA);
+
+out=['TAMB = ' num2str(TAMB) '°C, PAMB = ' num2str(PAMB) 'Pa, PTOTAL = ' num2str(PTOTAL) 'Pa, TTOTAL = ' num2str(TTOTAL) '°C, RHO = ' num2str(RHO) 'kg/m³, VSOUND = ' num2str(VSOUND) 'm/s, GAMMA = ' num2str(GAMMA) ', CP = ' num2str(CP) ', H = ' num2str(H)];
 
 disp(out)
 
