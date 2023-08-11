d35e329ba3c02d8af1a82e23921af7b858a81b1f
Tony Ruther <tony.ruther@googlemail.com>
2023-08-11 20:44:07 +0100

    components

--- DISP_AIRPROP.m
+++ DISP_AIRPROP.m
@@ -1,8 +1,8 @@
 #!/usr/bin/env octave -qf
 
-function DISP_AIRPROP(TAMB, PAMB, PTOTAL, TTOTAL, RHO, VSOUND, GAMMA, CP, H)
+function DISP_AIRPROP(STATION, TAMB, PAMB, PTOTAL, TTOTAL, RHO, VSOUND, GAMMA, CP, H)
 
-  out=['TAMB = ' num2str(TAMB) '°C, PAMB = ' num2str(PAMB) 'Pa, PTOTAL = ' num2str(PTOTAL) 'Pa, TTOTAL = ' num2str(TTOTAL) '°C, RHO = ' num2str(RHO) 'kg/m³, VSOUND = ' num2str(VSOUND) 'm/s, GAMMA = ' num2str(GAMMA) ', CP = ' num2str(CP) ', H = ' num2str(H)];
+  out=['STATION = ' num2str(STATION) ', TAMB = ' num2str(TAMB) '°C, PAMB = ' num2str(PAMB) 'Pa, PTOTAL = ' num2str(PTOTAL) 'Pa, TTOTAL = ' num2str(TTOTAL) '°C, RHO = ' num2str(RHO) 'kg/m³, VSOUND = ' num2str(VSOUND) 'm/s, GAMMA = ' num2str(GAMMA) ', CP = ' num2str(CP) ', H = ' num2str(H)];
 
   disp(out)
 
--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 #!/usr/bin/env octave -qf
 
-  
 arg_list=argv();
 alt=str2double(arg_list{1});
 xm=str2double(arg_list{2});
 dtamb=str2double(arg_list{3});
 
+station=0;
 
 [TAMB,PAMB,PTOTAL,TTOTAL,RHO,VSOUND,GAMMA,CP,H]=AIR_PROP(alt,xm,dtamb);
-DISP_AIRPROP(TAMB,PAMB,PTOTAL,TTOTAL,RHO,VSOUND,GAMMA,CP,H);
+DISP_AIRPROP(station,TAMB,PAMB,PTOTAL,TTOTAL,RHO,VSOUND,GAMMA,CP,H);
   
